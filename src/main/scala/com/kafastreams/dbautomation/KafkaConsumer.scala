27ee4c1548c370b61c9baff04d3ba5630442ad27
parameshnalla <sbpraonalla@gmail.com>
2019-05-08 17:10:52 +0530

    Feature kafka db automation paramesh (#1)
    * Kafka configuration
    
    * Kafka configuration
    
    * Kafka configuration
    
    * Kafka configuration

---
+++ src/main/scala/com/kafastreams/dbautomation/KafkaConsumer.scala
@@ -0,0 +1,20 @@
+package com.kafastreams.dbautomation
+
+/***
+  * Class to provide KafaConfiguration
+  */
+class KafkaConsumer {
+
+  // Basic server properties.
+
+
+  // Kafka consumer configurations
+
+
+  // reading the config from .conf file
+
+
+  // close kafka
+
+
+}
