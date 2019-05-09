f0302cfe121fd017fa195cf9770d65b20457c591
Paramesh Nalla <pnalla@tracelink.com>
2019-05-09 12:05:50 -0400

    Added .conf and added business logic for processing

--- src/main/scala/com/kafastreams/dbautomation/KafkaConsumer.scala
+++ src/main/scala/com/kafastreams/dbautomation/KafkaConsumer.scala
@@ -3,7 +3,6 @@ package com.kafastreams.dbautomation
 /** *
   * Class to provide KafaConfiguration
   */
-
 import org.apache.spark.sql.SparkSession
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
@@ -14,23 +13,22 @@ import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
 
-class KafkaConsumer {
+case class KafkaConsumer(
+                          topics: Array[String],
+                          kafkaParams: Map[String, Object],
+                          consumerName: String
+                        ) extends Thread {
 
-  def startKafkaSparkConsumer(topics: Array[String]) = {
+  override def run() = {
 
-    val spark = SparkSession.builder()
-      .master("local")
-      .appName("First Spark Job")
-      .getOrCreate()
-    val sc = spark.sparkContext
-    val ssc = new StreamingContext(sc, Seconds(1))
-    sc.setLogLevel("ERROR")
     val stream = KafkaUtils.createDirectStream[String, String](
-      ssc,
+      KafkaConsumer.ssc,
       PreferConsistent,
-      Subscribe[String, String](topics, KafkaConsumer.kafkaParams)
+      Subscribe[String, String](topics, kafkaParams)
     )
+    println(s"Consumer picked message: $consumerName")
     stream.foreachRDD(x =>
       if (!x.isEmpty()) {
         println("RDD is not empty !!!! count is " + x.count())
@@ -38,15 +36,8 @@ class KafkaConsumer {
         val key = x.map(record => record.key().asInstanceOf[String])
         val value = x.map(record => record.value().asInstanceOf[String])
 
-        val temp = value.map(y => {
-          y.replaceAll("Struct", "")
-            .replace("}", "")
-            .replace("{", "")
-        })
-        val temp2 = temp.map(
-          x => (x.split(",")(0), x.split(",")(1), x.split(",")(2), x.split(",")(3))
-        )
-        import spark.implicits._
+        val temp2 = value.map(x => x + s"$consumerName")
+        import KafkaConsumer.spark.implicits._
         val df = temp2.toDF()
         df.coalesce(1)
           .write.format("csv").mode("append").save("/Users/pnalla/myplayground/kafka/temp")
@@ -54,36 +45,19 @@ class KafkaConsumer {
       }
       else println("RDD is Empty")
     )
-    ssc.start()
-    ssc.awaitTermination()
+    KafkaConsumer.ssc.start()
+    KafkaConsumer.ssc.awaitTermination()
   }
 
 }
+object KafkaConsumer {
 
-object KafkaConsumer extends App {
-  val logger = Logger.getLogger(classOf[KafkaConsumer])
-
-  val config: Config = ConfigFactory.load()
-  val kafkaConsumerConfig = config.getConfig("kafkaProject.kafka_consumer_config")
-  val kafkaParams = Map[String, Object](
-    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> kafkaConsumerConfig
-      .getList("brokers")
-      .unwrapped()
-      .toArray()
-      .mkString(","),
-    ConsumerConfig.GROUP_ID_CONFIG -> kafkaConsumerConfig.getString("group_id"),
-    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> kafkaConsumerConfig.getString("auto_off_set_config"),
-    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (kafkaConsumerConfig.getBoolean("enable_auto_offset_commit"): java.lang.Boolean)
-  )
-  val topics = kafkaConsumerConfig
-    .getList("topics")
-    .unwrapped()
-    .toArray().map(_.toString)
-
-  logger.info(s"Starting kafkaConsumer with config: ${kafkaParams.mkString("\n")}")
-  logger.info(s"Working on the topics : $topics")
-
-
+  val spark = SparkSession.builder()
+    .master("local")
+    .appName("First Spark Job")
+    .getOrCreate()
+  val sc = spark.sparkContext
+  val ssc = new StreamingContext(sc, Seconds(1))
+  KafkaConsumer.sc.setLogLevel("ERROR")
+  ssc.checkpoint("checkpoint")
 }
--- src/main/scala/com/kafastreams/dbautomation/StartConsumerGroup.scala
+++ src/main/scala/com/kafastreams/dbautomation/StartConsumerGroup.scala
@@ -1,10 +1,56 @@
 package com.kafastreams.dbautomation
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.log4j.Logger
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
+import com.kafastreams.dbautomation.StartConsumerGroup.pool
+import kafka.server.KafkaConfig
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+import scala.concurrent.ExecutionContext
+
 
 object StartConsumerGroup extends  App {
 
-  val consumer1 = new KafkaConsumer
-  val consumer2 = new KafkaConsumer
-  consumer1.startKafkaSparkConsumer(KafkaConsumer.topics)
-  consumer2.startKafkaSparkConsumer(KafkaConsumer.topics)
 
+  val config: Config = ConfigFactory.load()
+  val kafkaConsumerConfig = config.getConfig("kafkaProject.kafka_consumer_config")
+
+  val kafkaParams = Map[String, Object](
+    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> kafkaConsumerConfig
+      .getList("brokers")
+      .unwrapped()
+      .toArray()
+      .mkString(","),
+    ConsumerConfig.GROUP_ID_CONFIG -> kafkaConsumerConfig.getString("group_id"),
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> kafkaConsumerConfig.getString("auto_off_set_config"),
+    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (kafkaConsumerConfig.getBoolean("enable_auto_offset_commit"): java.lang.Boolean)
+  )
+  val topics = kafkaConsumerConfig
+    .getStringList("topics")
+    .toArray()
+    .map(_.toString)
+
+  val consumers = Array(
+    KafkaConsumer(topics,kafkaParams,"consumer1"),
+    KafkaConsumer(topics,kafkaParams,"consumer2")
+  )
+
+
+  val numberOfConsumerToSpin = kafkaConsumerConfig.getInt("number_consumers_to_start")
+  val consumerList = (1 to numberOfConsumerToSpin).toList
+  val pool: ExecutorService = Executors.newFixedThreadPool(numberOfConsumerToSpin)
+  consumerList.foreach( consumer =>
+    pool.execute(KafkaConsumer(topics,kafkaParams,s"consumer-$consumer"))
+  )
+  pool.shutdown()
 }
