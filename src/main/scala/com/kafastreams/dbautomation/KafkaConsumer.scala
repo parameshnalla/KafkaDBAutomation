73b849b91968dc81c044c41906bf8662b4093184
Paramesh Nalla <pnalla@tracelink.com>
2019-05-13 19:14:53 +0000

    Added .conf and added business logic for processing

--- src/main/scala/com/kafastreams/dbautomation/KafkaConsumer.scala
+++ src/main/scala/com/kafastreams/dbautomation/KafkaConsumer.scala
@@ -5,11 +5,13 @@ package com.kafastreams.dbautomation
   */
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.{CanCommitOffsets, HasOffsetRanges, KafkaUtils}
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.Seconds
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+import scala.util.Try
 
 
 case class KafkaConsumer(
@@ -21,30 +23,67 @@ case class KafkaConsumer(
 
   override def run() = {
 
-    val stream = KafkaUtils.createDirectStream[String, String](
-      KafkaSpark.ssc,
-      PreferConsistent,
-      Subscribe[String, String](topics, kafkaParams)
-    )
-   // println(s"Consumer picked message: $consumerName")
-    stream.foreachRDD(x =>
-      if (!x.isEmpty()) {
-        println("RDD is not empty !!!! count is " + x.count())
+    var isStopped = false
 
-        val key = x.map(record => record.key().asInstanceOf[String])
-        val value = x.map(record => record.value().asInstanceOf[String])
-
-        val temp2 = value.map(x => x + s"$consumerName")
-        import KafkaSpark.spark.implicits._
-        val df = temp2.toDF()
-        df.coalesce(1)
-          .write.format("csv").mode("append").save("/Users/pnalla/myplayground/kafka/temp")
-        df.show(100, false)
+    /*def gracefulShutDown() = {
+      while(!isStopped){
+         isStopped = KafkaSpark.ssc.awaitTerminationOrTimeout(1000)
+        if(isStopped){
+          println("Stoping graceful shutdown")
+          KafkaSpark.ssc.stop(true, true)
+          println("Stoping graceful shutdown completed")
+        }
       }
-      //else  println("RDD is Empty")
-    )
-    KafkaSpark.ssc.start()
-    KafkaSpark.ssc.awaitTermination()
+    }*/
+
+    def processStream(): Unit = {
+      val stream = KafkaUtils.createDirectStream[String, String](
+        KafkaSpark.ssc,
+        PreferConsistent,
+        Subscribe[String, String](topics, kafkaParams)
+      )
+
+      try {
+        stream.foreachRDD(records =>
+          if (!records.isEmpty()) {
+            println("RDD is not empty !!!! count is " + records.count())
+
+            val offsetRanges = records.asInstanceOf[HasOffsetRanges].offsetRanges
+
+            val key = records.map(record => record.key().asInstanceOf[String])
+            val value = records.map(record => record.value().asInstanceOf[String])
+
+            val recordValue = value.map(x => x + s"$consumerName")
+            import KafkaSpark.spark.implicits._
+            val df = recordValue.toDF()
+            df.coalesce(1)
+              .write
+              .format("csv")
+              .mode("append")
+              .save("/Users/pnalla/myplayground/kafka/temp")
+            df.show(100, false)
+            stream.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
+            println(s"OffsetRanges commited \n :${offsetRanges.toList.mkString("\n")}")
+          }
+          else println("RDD is Empty")
+        )
+        KafkaSpark.ssc.start()
+      } catch {
+        case _: NullPointerException => println("Stream is empty . Retrying ....")
+        case _: java.lang.IllegalStateException => println("Stream not stopped correctly. Retrying ....")
+        case ex: Exception => println(s"Generic exception occured \n ${ex.getMessage} \n ${ex.getStackTrace}")
+      }
+
+    }
+    processStream()
+    KafkaSpark.ssc.awaitTerminationOrTimeout(20000)
+    Runtime.getRuntime.addShutdownHook(new Thread() {
+      override def run(): Unit = {
+        System.out.println(s"Shutting down streaming app... $consumerName")
+        KafkaSpark.ssc.stop(true, true)
+        System.out.println(s"Shutdown of streaming app complete. $consumerName")
+      }
+    })
   }
 
 }
--- src/main/scala/com/kafastreams/dbautomation/KafkaSpark.scala
+++ src/main/scala/com/kafastreams/dbautomation/KafkaSpark.scala
@@ -1,5 +1,6 @@
 package com.kafastreams.dbautomation
 
+import com.kafastreams.dbautomation.StartConsumerGroup.config
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -9,7 +10,10 @@ object KafkaSpark {
     .appName("First Spark Job")
     .getOrCreate()
   val sc = spark.sparkContext
-  val ssc = new StreamingContext(sc, Seconds(1))
+  val ssc = new StreamingContext(sc, Seconds(
+    StartConsumerGroup
+      .config
+      .getInt("kafkaProject.spark_config.poll_interval")))
   sc.setLogLevel("ERROR")
   ssc.checkpoint("checkpoint")
 }
